b55ebc873340f769a3d8e2a48e6a715761ef54d1
bluemoon <bluemoon@localhost.localdomain>
2019-11-18 08:22:25 -0500

    MacChanger

--- MACChanger.py
+++ MACChanger.py
@@ -4,19 +4,21 @@ import subprocess
 import optparse
 
 
+def get_arguments():
+	parser = optparse.OptionParser()
+	parser.add_option("-i" , "--interface", dest = "interface", help="Add the interface of changing MAC")
+	parser.add_option("-m" , "--mac", dest = "new_mac", help="Add the mac of changing MAC")
+	(options, arguments) = parser.parse_args()
+	if not options.interface:
+		parser.error("[-] Kindly specify the Interface, or use --help  for more info")
+	elif not options.new_mac:
+		parser.error("[-] Kindly specify the Mac Address, or use --help  for more info")
+	return options
 def mac_change(interface, new_mac):
 	print("[+] Changing MAC Address for " + interface +" to "+new_mac)
 	subprocess.call("ifconfig "+ interface+ " down", shell=True)
 	subprocess.call("ifconfig "+ interface+ " hw ether 00:11:22:33:44:66", shell=True)
 	subprocess.call("ifconfig "+ interface+ " up", shell=True)
-
-parser = optparse.OptionParser()
-parser.add_option("-i" , "--interface", dest = "interface", help="Add the interface of changing MAC")
-parser.add_option("-m" , "--mac", dest = "new_mac", help="Add the mac of changing MAC")
-
-(options, arguments) = parser.parse_args()
-
-interface =options.interface
-new_mac = options.new_mac
-
+ 
+options= get_arguments()
 mac_change(options.interface,options.new_mac) 	
